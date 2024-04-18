a84bf4c0fbe52e7dd46d5304b46e1cfe3d7dccb0
Doumbia Alhassan <hassan4cod@gmail.com>
2024-04-18 20:13:01 +0000

    refactored the getCurrentPositionWeather() functions , fullfiled the promise feature

--- utilitary_Functions.js
+++ utilitary_Functions.js
@@ -52,27 +52,63 @@ export async function getWeatherByCords(lat, long) {
   }
 }
 
+// export function getCurrentPositionWeather() {
+//   if ("geolocation" in navigator) {
+//     try {
+//       /*Debug*/ console.log("recherche de la longitude et de la lattitude ");
+//       navigator.geolocation.getCurrentPosition((positon) => {
+//         getWeatherByCords(positon.coords.latitude, positon.coords.longitude);
+//       }),
+//         (error) => {
+//           console.log(
+//             `il ya une erreur ${error.code}, qui signifie ${error.message}`
+//           );
+//         };
+//     } catch (error) {
+//       console.log(error);
+//       //Va integrer des données dummy dans l'interface si il ya erreur
+//     }
+//   } else {
+//     console.log("l'API geolocation n'est pas disponible sur votre naviigteur");
+//     alert("la géolocalisation n'est pas disponible sur votre ordinateur ");
+//     // L'on ferra afficher des données Dummy
+//   }
+// }
+
 export function getCurrentPositionWeather() {
-  if ("geolocation" in navigator) {
-    try {
-      /*Debug*/ console.log("recherche de la longitude et de la lattitude ");
-      navigator.geolocation.getCurrentPosition((positon) => {
-        getWeatherByCords(positon.coords.latitude, positon.coords.longitude);
-      }),
-        (error) => {
-          console.log(
-            `il ya une erreur ${error.code}, qui signifie ${error.message}`
-          );
-        };
-    } catch (error) {
-      console.log(error);
-      //Va integrer des données dummy dans l'interface si il ya erreur
+  return new Promise((resolve, reject) => {
+    if ("geolocation" in navigator) {
+      try {
+        console.log("Recherche de la longitude et de la latitude");
+        navigator.geolocation.getCurrentPosition(
+          (positon) => {
+            const weatherData = getWeatherByCords(
+              positon.coords.latitude,
+              positon.coords.longitude
+            );
+            resolve(weatherData);
+          },
+          (error) => {
+            console.log(
+              `Il y a une erreur ${error.code}, qui signifie ${error.message}`
+            );
+            reject(error);
+          }
+        );
+      } catch (error) {
+        console.log(error);
+        reject(error);
+        // Va intégrer des données dummy dans l'interface si il y a une erreur
+      }
+    } else {
+      console.log(
+        "L'API geolocation n'est pas disponible sur votre navigateur"
+      );
+      alert("La géolocalisation n'est pas disponible sur votre ordinateur");
+      reject("Géolocalisation non disponible");
+      // L'on fera afficher des données Dummy
     }
-  } else {
-    console.log("l'API geolocation n'est pas disponible sur votre naviigteur");
-    alert("la géolocalisation n'est pas disponible sur votre ordinateur ");
-    // L'on ferra afficher des données Dummy
-  }
+  });
 }
 
 // fonction permettant de creer l'historique
@@ -91,33 +127,34 @@ export function RecordInCityHistory(citiesArray, cityName) {
 
 export function currentWeatherDisplay() {
   /*debug*/ console.log("affichage de la météo selon la position géographique");
-  const response = getCurrentPositionWeather(); // fonctionne
-  // const weatherObject = JSON.parse(response); //pour transformer en objet
-  // en fait on est pas obligé de transformer en objet
-  //on va commencer à display les informations maintenant
+  getCurrentPositionWeather().then((response) => {
+    /*debug*/ console.log(`voilà les données ${response}`);
 
-  /*debug*/ console.log(`voilà les données ${response}`);
-
-  weather.innerText = response.weather[0].main;
-  cityInfo.innerText = `${response.sys.name},${response.sys.country}`;
-  infoDisplay(
-    response.main.temp,
-    weatherObject.main.humidity,
-    "???",
-    response.wind.speed,
-    response.main.humidity,
-    response.main.visibility
-  );
-  // modification du background et des icone en fonction de la prévision
-  switch (response.weather[0].main) {
-    case "clouds":
-      // document.body.style.backgroundImage="url('../assets/images/Cloudy) à utiliser plus tard
-      temperatureIcon.setAttribute("src", "assets/icones/icons8-rain-96.png");
-      weatherIndicator.setAttribute("src");
-      break;
-    case "clear":
-      // document.body.style.backgroundImage="url('../assets/images/Cloudy) à utiliser plus tard
-      temperatureIcon.setAttribute("src", "assets/icones/icons8-summer-96.png");
-      break;
-  }
+    weather.innerText = response.weather[0].main;
+    cityInfo.innerText = `${response.sys.name},${response.sys.country}`;
+    infoDisplay(
+      response.main.temp,
+      response.main.humidity,
+      "???",
+      response.wind.speed,
+      response.main.humidity,
+      response.visibility,
+      response.main.lightInt
+    );
+    // modification du background et des icone en fonction de la prévision
+    switch (response.weather[0].main) {
+      case "clouds":
+        // document.body.style.backgroundImage="url('../assets/images/Cloudy) à utiliser plus tard
+        temperatureIcon.setAttribute("src", "assets/icones/icons8-rain-96.png");
+        weatherIndicator.setAttribute("src");
+        break;
+      case "clear":
+        // document.body.style.backgroundImage="url('../assets/images/Cloudy) à utiliser plus tard
+        temperatureIcon.setAttribute(
+          "src",
+          "assets/icones/icons8-summer-96.png"
+        );
+        break;
+    }
+  });
 }

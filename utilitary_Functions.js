b796c40ad0d73faeeb512fe1292d904c0c6cd3a6
Doumbia Alhassan <hassan4cod@gmail.com>
2024-04-25 23:30:57 +0000

    created a new function for the diagnostic ,using chatGPT API , integrated in the InfoDisplay function

--- utilitary_Functions.js
+++ utilitary_Functions.js
@@ -11,7 +11,8 @@ const temperatureIcon = document.querySelector("#icon");
 const weatherIndicator = document.querySelector("#weather");
 const diagnosticVerdict = document.querySelector(".diagnostic_verdict");
 
-const temperature = document.querySelectorAll(".temperature");
+const temperature = document.getElementsByClassName("temperature");
+const mainTemperature = document.getElementById("main_Temp");
 const temperature2 = document.getElementById("temp");
 const humidity = document.getElementById("humidity");
 const precipitation = document.getElementById("precipitaion");
@@ -23,34 +24,10 @@ const weather = document.getElementById("weather");
 const cityInfo = document.getElementById("cityInfo");
 const HistoryDisplay = document.querySelector(".historyDisplay");
 const noDataStatement = document.getElementById("noData");
-const atmos_pressure = document.getElementById("atmos_pressure");
+const background = document.getElementById("background");
+
+const verdict = document.querySelector(".diagostic_explanation");
 //Fin variables
-
-function infoDisplay(
-  temp,
-  humi,
-  windInt,
-  visib,
-  weatheForecast,
-  pressure,
-  city
-) {
-  try {
-    parseFloat(temp);
-    var temp = Math.round(temp - 273.15); // Kelvin to celcius
-
-    temperature[1].textContent = `${temp} °C`;
-    temperature[0].textContent = `${temp} °C`;
-    humidity.innerText = `${humi} %`;
-    wind.innerText = `${windInt} m/s`;
-    visibility.innerText = `${visib} m`;
-    weather.innerText = `${weatheForecast}`;
-    atmos_pressure.innerText = `${pressure} hPa`;
-    cityInfo.innerText = `${city}`;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 export function getWeather(city) {
   return new Promise((resolve, reject) => {
@@ -102,32 +79,118 @@ export async function cityWeatherDisplay(city) {
         cityWeatherData.main.pressure,
         city
       );
+
+      /** debug */ console.log(
+        `modification du Background , type : ${cityWeatherData.weather[0].main}`
+      );
+      switch (cityWeatherData.weather[0].main) {
+        case "clouds":
+          background.style.backgroundImage =
+            "url('assets/images/cloudy_sky.jpg')";
+          temperatureIcon.setAttribute(
+            "src",
+            "assets/icones/icons8-rain-96.png"
+          );
+          break;
+        case "clear":
+          background.style.backgroundImage =
+            "url('assets/images/clear_sky.jpg')";
+          temperatureIcon.setAttribute(
+            "src",
+            "assets/icones/icons8-summer-96.png"
+          );
+          break;
+      }
     });
   } catch (error) {
     /**debug */ console.error(error);
   }
 }
 
-/** Utiliser ce bout de code dans la fonction cityWeatherDisplay 
- * 
- * // modification du background et des icone en fonction de la prévision
-        switch (response2.weather[0].main) {
-          case "clouds":
-            // document.body.style.backgroundImage="url('../assets/images/Cloudy) à utiliser plus tard
-            temperatureIcon.setAttribute(
-              "src",
-              "assets/icones/icons8-rain-96.png"
-            );
-            weatherIndicator.setAttribute("src");
-            break;
-          case "clear":
-            // document.body.style.backgroundImage="url('../assets/images/Cloudy) à utiliser plus tard
-            temperatureIcon.setAttribute(
-              "src",
-              "assets/icones/icons8-summer-96.png"
-            );
-            break;
+/**Fonction de chatGPT
+ * L'API que j'utiliserai est lente asf ,mais bon c'est pour les test
+ */
+
+// Comme paramètre , il recevra les données de temperatures etc et il est intégré à infoDisplay plus haut
+function getVerdict(temp, humi, windInt, visib, weatheForecast, pressure) {
+  let needed_content = `You are a weather AI designed for drone enthusiasts. You provide recommendations on whether it\'s safe to fly based on data, offering clear explanations for your decisions. what do you think of those  conditions ? ${humi},${temp},${windInt},${visib},${weatheForecast},${pressure},try to write your diagnostic in 4line max`;
+  return new Promise((resolve, reject) => {
+    const url = "https://open-ai21.p.rapidapi.com/chatgpt";
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key":
+          "45539c1454mshe2988be1e97c2cfp173326jsn950d6de2fb8f" /**Eviter d'utiliser l'API sauf en demo */,
+        "X-RapidAPI-Host": "open-ai21.p.rapidapi.com",
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: "user",
+            content: needed_content,
+          },
+        ],
+        web_access: false,
+      }),
+    };
+    fetch(url, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Réponse d'API non réussie : ${response.statusText}`);
         }
-        
-        
-        il s'agit du code permettant de modifier le background / */
+        return response.json();
+      })
+      .then((data) => resolve(data))
+      .catch((error) => {
+        console.log(
+          `il ya une erreur au niveau de la fonction de GPT  : ${error}`
+        );
+        reject(error);
+      });
+  });
+}
+
+async function infoDisplay(
+  temp,
+  humi,
+  windInt,
+  visib,
+  weatheForecast,
+  pressure,
+  city
+) {
+  try {
+    parseFloat(temp);
+    var temp = Math.round(temp - 273.15); // Kelvin to celcius
+
+    temperature[1].textContent = `${temp} °C`;
+    temperature[0].textContent = `${temp} °C`;
+    humidity.innerText = `${humi} %`;
+    wind.innerText = `${windInt} m/s`;
+    visibility.innerText = `${visib} m`;
+    weather.innerText = `${weatheForecast}`;
+    atmos_pressure.innerText = `${pressure} hPa`;
+    cityInfo.innerText = `${city}`;
+
+    await getVerdict(
+      temp,
+      humi,
+      windInt,
+      visib,
+      weatheForecast,
+      pressure,
+      city
+    ).then((response) => {
+      /**debug */ console.log(JSON.stringify(response));
+      verdict.innerHTML = response.result;
+    });
+
+    /**Animation
+     */
+    // temperature[0].classList.remove("temperatureSideOut");
+    // temperature[0].classList.add("temperatureSideIn");
+  } catch (error) {
+    console.error(error);
+  }
+}

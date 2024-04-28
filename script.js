a17af06d9faec858f5c108383e0c77d16ca79ff9
Doumbia Alhassan <hassan4cod@gmail.com>
2024-04-28 11:35:22 +0000

    deleted unnecessary const and comments , upgraded the error management

--- script.js
+++ script.js
@@ -1,73 +1,45 @@
+/*
+  Created by Doumbia Al Hassan, AKA Caprisunpapy
+  Abidjan, Côte d'Ivoire
+*/
+
 import { cityWeatherDisplay } from "./utilitary_Functions.js";
-
 // variables
-const searchField = document.querySelector("#SearchZone");
+const SearchInput = document.querySelector("#SearchZone");
 const sendButton = document.querySelector(".send");
-const diagnosticExplanation = document.querySelector(".diagnostic_explanation");
 
-const temperatureIcon = document.querySelector("#icon");
-const weatherIndicator = document.querySelector("#weather");
-const diagnosticVerdict = document.querySelector(".diagnostic_verdict");
-
-const temperature = document.getElementsByClassName("temperature");
-const mainTemperature = document.getElementById("main_Temp");
-const temperature2 = document.getElementById("temp");
-const humidity = document.getElementById("humidity");
-const precipitation = document.getElementById("precipitaion");
-const wind = document.getElementById("wind");
-const visibility = document.getElementById("visibility");
-const lightIntensity = document.getElementById("light_intensity");
-
-const weather = document.getElementById("weather");
-const cityInfo = document.getElementById("cityInfo");
 const HistoryDisplay = document.querySelector(".historyDisplay");
 const noDataStatement = document.getElementById("noData");
-const background = document.getElementById("background");
 //Fin variables
 
-//currentPositon weather function to get  the user current position  and then
-//dislay the user current position Weather Inof
-
-// function infoDisplay(temp, humi, windInt, visib) {
-//   temperature[1].textContent = temp;
-//   temperature[0].textContent = `${temp}°C`;
-//   humidity.innerText = humi;
-//   wind.innerText = windInt;
-//   visibility.innerText = visib;
-// }
-
-// Manipulatio de l'historique sinon on passera notre temps à reinitialiser l'historique
+// History
 let citiesHistory = [];
 const historyContainer = document.createElement("ul");
-//Tableau de l'historique
 
 window.addEventListener("load", () => {
   cityWeatherDisplay("Abidjan");
-  displayVerdict();
 });
-
-//le code à partir du else ligne 142
 
 sendButton.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("la bouton envoyé a été clické ");
-  let searchFieldContent = document.querySelector("#SearchZone").value; // J'avais mis value. Faudrit que je recherche comment recuperer le contenu d'un input d etype text
-  if (searchFieldContent === "") {
+  let SearchInputContent = document.querySelector("#SearchZone").value;
+  if (SearchInputContent === "") {
     try {
       cityWeatherDisplay("Abidjan");
     } catch (error) {
-      /**JE VAIS APPLIQUER ICI MA NOUVELLE FONCTION */
+      console.error(
+        "Erreur lors de l'affichage de la météo pour la ville par defaut(Abidjan):",
+        error
+      );
     }
   } else {
-    //à factoriser
     try {
-      // temperature[0].classList.remove("temperatureSideIn");
-      // temperature[0].classList.add("temperatureSideOut");
-      cityWeatherDisplay(searchFieldContent);
+      cityWeatherDisplay(SearchInputContent);
       let li = document.createElement("li");
-      li.textContent = searchFieldContent;
+      li.textContent = SearchInputContent;
       citiesHistory.push(li);
-      searchField.value = ""; //reinitialiser l'input
+      SearchInput.value = "";
       /*debug*/ console.log(
         "le champs est vidé, prêt pour une nouvelle requête"
       );
@@ -81,34 +53,46 @@ sendButton.addEventListener("click", (e) => {
         }
       }
     } catch (error) {
-      console.log(
-        `erreur au niveau du code principal ,l'erreur retourné est la suivante : ${error}`
+      console.error(
+        "Erreur lors de l'affichage de la météo pour la ville recherchée:",
+        error
       );
     }
   }
 });
 
-// event si on presse entrer , on lance une requête
-
-searchField.addEventListener("keydown", function (e) {
+SearchInput.addEventListener("keydown", function (e) {
   if (e.keycode === 13) {
     // debug feature
+
     console.log("le bouton entrer a été pressé ");
     e.preventDefault();
-    let searchFieldContent = document.querySelector("#searchZone").value;
-    if (searchFieldContent === "") {
-      cityWeatherDisplay("Abidjan");
+    let SearchInputContent = document.querySelector("#searchZone").value;
+    if (SearchInputContent === "") {
+      try {
+        cityWeatherDisplay("Abidjan");
+      } catch (error) {
+        console.error(
+          "Erreur lors de l'affichage de la météo pour la ville par defaut(Abidjan):",
+          error
+        );
+      }
     } else {
-      temperature[0].classList.remove("temperatureSideIn");
-      temperature[0].classList.add("temperatureSideOut");
-      cityWeatherDisplay(searchFieldContent);
-      let li = document.createElement("li");
-      li.textContent = searchFieldContent;
-      citiesHistory.push(li);
-      searchField.value = ""; //reinitialiser l'input
-      /*debug*/ console.log(
-        "le champs est vidé, prêt pour une nouvelle requête"
-      );
+      try {
+        cityWeatherDisplay(SearchInputContent);
+        let li = document.createElement("li");
+        li.textContent = SearchInputContent;
+        citiesHistory.push(li);
+        SearchInput.value = "";
+        /*debug*/ console.log(
+          "le champs est vidé, prêt pour une nouvelle requête"
+        );
+      } catch (error) {
+        console.error(
+          "Erreur lors de l'affichage de la météo pour la ville recherchée:",
+          error
+        );
+      }
 
       if (citiesHistory.length === 0) {
         noDataStatement.style.display = "block";
@@ -118,8 +102,6 @@ searchField.addEventListener("keydown", function (e) {
         for (let i = 0; i < citiesHistory.length; i++) {
           historyContainer.appendChild(citiesHistory[i]);
         }
-
-        // création de l'historique+verification
         if (citiesHistory.length === 0) {
           noDataStatement.style.display = "block";
         } else {
